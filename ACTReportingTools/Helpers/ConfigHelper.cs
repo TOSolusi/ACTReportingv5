0b6a0abb8d353065fe0600099a7656e8cc50360f
Fenky Tjhang <cornelis@hotmail.com>
2025-01-13 22:28:14 +0700

    settings loading
    Done

---
+++ ACTReportingTools/Helpers/ConfigHelper.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Syncfusion.Compression.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ACTReportingTools.Helpers
+{
+    public class ConfigHelper
+    {
+       
+        public static JObject LoadConfig(string FileName)
+        {
+            //IConfiguration configuration = new ConfigurationBuilder()
+            //  .AddJsonFile("Config/reportsettings.json", true, true)
+            //  .AddEnvironmentVariables()
+            //  .Build();
+
+            //JObject config = JObject.Parse(File.ReadAllText("Config/reportsettings.json"));
+
+
+            // read JSON directly from a file
+
+            JObject o2 = new JObject();
+
+            using (StreamReader file = File.OpenText($"Settings/{FileName}"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                o2 = (JObject)JToken.ReadFrom(reader);
+            }
+
+            return o2;
+        }
+
+        public static void SetConfig(JObject config, string FileName)
+        {
+            try
+            {
+                //string filename = "Config/reportsettings.json";
+                //FileStream fs = null;
+                //fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write);
+                //using (StreamWriter file = new StreamWriter(fs))
+
+                //using (StreamWriter file = File.CreateText("Config/reportsettings.json")) 
+                //using (var writer = new JsonTextWriter(file))
+                //{
+                //   config.WriteTo(writer);
+                //}
+
+                var jsonString = JsonConvert.SerializeObject(config, formatting: Newtonsoft.Json.Formatting.Indented);
+                
+                File.WriteAllText("Settings/{FileName}", jsonString);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
--- ACTReportingTools/ViewModels/ReportSettingsViewModel.cs
+++ ACTReportingTools/ViewModels/ReportSettingsViewModel.cs
@@ -1,20 +1,79 @@
-﻿using Caliburn.Micro;
+﻿using ACTReportingTools.Helpers;
+using Caliburn.Micro;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+
 
 namespace ACTReportingTools.ViewModels
 {
     public class ReportSettingsViewModel : Screen
     {
-        public MenuViewModel menuViewModel { get; set; }         
+        public MenuViewModel menuViewModel { get; set; }
+
+        public JObject _config { get; set; }
+
+        public ObservableCollection<string> HourNumbers { get; set; }
+        public ObservableCollection<string> MinuteNumbers { get; set; }
+        public ObservableCollection<string> DurationNumbers { get; set; }
+        public string TimeInHourFromString { get; set; }
+        public string TimeInMinuteFromString { get; set; }
         public ReportSettingsViewModel()
         {
                 menuViewModel = IoC.Get<MenuViewModel>();
+            string SettingsFileName = "ReportSettings.json";
+
+            HourNumbers = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
+            MinuteNumbers = ["00", "15", "30", "45"];
+            DurationNumbers = ["00", "15", "30", "45", "60", "75", "90", "105", "120"];
+
+            _config = ConfigHelper.LoadConfig(SettingsFileName);
+            TimeInFrom = (string)_config["TimeInFrom"];
+            TimeInTo = (string)_config["TimeInTo"];
+
+            TimeInHourFromString = "08";
+            TimeInMinuteFromString = "00";
+            //TimeInFrom = "08:00";
+            //TimeInTo = "10:00";
+            //HourNumber = (string) _config["TimeInFrom"];
+            //TimeInFrom = TimeOnly.Parse((string)_config["TimeInFrom"]);
+            //TimeInTo = TimeOnly.Parse((string) _config["TimeInTo"]);  
+
         }
 
+
+
+        private string timeInFrom;
+
+        public string TimeInFrom
+        {
+            get { return timeInFrom; }
+            set 
+            { 
+                timeInFrom = value;
+                NotifyOfPropertyChange(() => TimeInFrom);
+            }
+        }
+
+        private string timeInTo;
+
+        public string TimeInTo
+        {
+            get { return timeInTo; }
+            set
+            {
+                timeInTo = value;
+                NotifyOfPropertyChange(() => TimeInTo);
+            }
+        }
+
+
+       
         public void ButtonCancel()
         {
             menuViewModel.ButtonReportDate();
